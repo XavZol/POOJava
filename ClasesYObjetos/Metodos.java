07b2d5e24a85b809f43455adfd54398c5136d16a
XavZol <alansolis707@gmail.com>
2026-02-27 19:20:01 -0600

    Miembros estáticos de una clase 2.

--- ClasesYObjetos/Metodos.java
+++ ClasesYObjetos/Metodos.java
@@ -2,16 +2,16 @@
 
 public class Metodos {
 
-    private String frase = "Primera frase";
+    private static String frase = "Primera frase";
+
+    public static int sumar(int n1,int n2 ){
+        int suma = n1+n2;
+        return suma;
+    }
     // Atributos
     public static void main(String[] args) {
-        Metodos ob1 = new Metodos();
-        Metodos ob2 = new Metodos();
-
-        ob2.frase = "Segunda frase";
-
-        System.out.println(ob1.frase);
-        System.out.println(ob2.frase);
+        System.out.println(Metodos.frase);
+        System.out.println("La suma es: "+Metodos.sumar(3, 4));
     }
 
 }
